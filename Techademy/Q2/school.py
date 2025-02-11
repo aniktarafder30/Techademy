5ef2bfea849382ff4d0992557bb3ab39884828a2
aniktarafder30 <anik.tarafder.30@gmail.com>
2025-02-11 22:28:49 +0800

    modified git link

--- Techademy/Q2/school.py
+++
@@ -1,9 +0,0 @@
-from student import Student
-
-class School(Student):
-    def __init__(self, name, grade, age):
-        super().__init__(name, grade, age)
-
-    def SchoolStudentDisplay(self):
-        print("School Student Details:")
-        self.display()
